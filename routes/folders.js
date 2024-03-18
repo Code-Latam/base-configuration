7a3da4b3ced548978113456b82dc3c38a8bb4cd1
stevenJonathanB <119703436+stevenJonathanB@users.noreply.github.com>
2024-03-18 15:19:29 -0500

    Enabled sync of folders with APIS
    Enabled sync of folders with APIS

--- routes/folders.js
+++ routes/folders.js
@@ -1,6 +1,8 @@
 const Client = require("../models/Client");
 const Folder = require("../models/Folder");
+const Api = require("../models/Api");
 const utils = require("../utils/utils.js");
+const folderutils = require("../utils/folderutils.js");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
@@ -149,7 +151,7 @@ router.post("/delete", async (request, res) => {
     }
 });
 
-// Query one explorer
+
 router.post("/query", async (request, res) => {
 
   const req = await utils.getDecodedBody(request);
@@ -191,6 +193,104 @@ router.post("/query", async (request, res) => {
 });
 
 
+router.post("/sync", async (request, res) => {
+
+   const req = await utils.getDecodedBody(request);
+ 
+   if (!req.endtoendPass)
+      {
+       res.status(401).json(utils.Encryptresponse(req.encryptresponse,"End to end encryption required or end to end encryption not correct",req.body.apiPublicKey));
+       return
+      }  
+ 
+   if (!req.gwokenPass)
+      {
+       res.status(401).json(utils.Encryptresponse(req.encryptresponse,"Gwoken required or GWOKEN calculation not correct",req.body.apiPublicKey));
+       return
+      }  
+ 
+ 
+      if (!req.body.clientNr)
+      {
+       res.status(412).json(utils.Encryptresponse(req.encryptresponse,"ClientNr is a required field",req.body.apiPublicKey));
+       return
+      }  
+ 
+      const client = await Client.findOne({ clientNr: req.body.clientNr })
+      if (!client)
+       {
+        res.status(401).json(utils.Encryptresponse(req.encryptresponse,"clientNr does not exist",req.body.apiPublicKey));
+        return
+       } 
+   try {
+     var items;
+     const folders = await Folder.findOne({ clientNr: req.body.clientNr}, { _id: 0 });
+     if (!folders) 
+     { // no folders found. Create the folders
+
+         const documentStructure = {
+            clientNr: req.body.clientNr,
+            items: {
+            root: {
+               index: "root",
+               isFolder: true,
+               children: ["MyFolders", "Unassigned"],
+               data: "Root item"
+            },
+            Unassigned: {
+               index: "Unassigned",
+               isFolder: true,
+               children: [],
+               data: "Unassigned Apis"
+            },
+            MyFolders: {
+               index: "MyFolders",
+               isFolder: true,
+               children: [],
+               data: "MyFolders"
+            },
+            },
+         };
+      
+       const newFolder = new Folder(documentStructure);
+       await newFolder.save();
+       items = documentStructure.items
+     }
+     else
+     {
+        items = folders.items
+     }
+
+
+     // // fetch apis and sync the folder structure with them
+     var myNewItems = items
+     const Apis = await Api.find({ clientNr: req.body.clientNr});
+     if (Apis)
+      {
+         Apis.forEach(api => {
+            const apiName = api.name;
+            const apifound = folderutils.findApiInFolder(items, apiName);
+            if (!apifound)
+            {
+               myNewItems = folderutils.addChildToFolder(apiName, "Unassigned", myNewItems );
+            }
+        
+          });
+      }
+
+      console.log("MY NEW ITEMS");
+      console.log(myNewItems);
+      await Folder.findOneAndUpdate({clientNr:req.body.clientNr}, { $set: { items: myNewItems } })
+      res.status(200).json(utils.Encryptresponse(req.encryptresponse,"SYNC COMPLETED",req.body.apiPublicKey))
+
+     }
+     catch (err) {
+       res.status(500).json(utils.Encryptresponse(req.encryptresponse,"An internal server error ocurred. Please check your fields",req.body.apiPublicKey))
+   }
+ });
+
+ 
+ 
 
 
 module.exports = router;
